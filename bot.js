f2c23793ccae89cf61b43a9443ad5f8a224a2ae9
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-17 19:12:16 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,77 +1,37 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '_'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`_bc RGM CLAN `,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+const botStats = {
+	totalGuildsID: '468779433692758016',
+	totalUsersID: '468779034302480384',
+	totalChannelsID: '468779680632274963',
+	
+	
+}
+	
+	client.on('guildCreate', guild => {
+		
+	client.channels.get(botStats.totalGuildsID).setname('TotalGuilds : ${client.guilds.size}');
+	client.channels.get(botStats.totalUsersID).setname('TotalUsers : ${client.guilds.reduce((a, g) => a + g.memberCount, 0)}');
+	client.channels.get(botStats.totalChannelsID).setname('TotalChannels : ${client.channels.size}');
+		
+		
+		
 });
-
- client.on('message', message => {
-              if(!message.channel.guild) return;
-    var prefix = "_";
-    if(message.content.startsWith('_bc')) {
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "RGM BC Bot";
-    let request = `Requested By ${message.author.username}`;
-    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-    msg.react('✅')
-    .then(() => msg.react('❌'))
-    .then(() =>msg.react('✅'))
-
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-    reaction1.on("collect", r => {
-    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
-    message.guild.members.forEach(m => {
-    var bc = new
-       Discord.RichEmbed()
-       .setColor('RANDOM')
-       .setTitle('Broadcast')
-       .addField('Server', message.guild.name)
-       .addField('Sender', message.author.username)
-       .addField('Message', args)
-       .setImage("https://cdn.discordapp.com/attachments/466685842833014789/466696993075494913/image.jpg")
-       .setThumbnail(message.author.avatarURL)
-       .setFooter(copy, client.user.avatarURL);
-    m.send({ embed: bc })
-    msg.delete();
-    })
-    })
-    reaction2.on("collect", r => {
-    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
-    msg.delete();
-    })
-    })
-    }
-    });
+		
+		
+	client.on('guildDelete', guild => {
+	
+	
+	client.channels.get(botStats.totalGuildsID).setname('TotalGuilds : ${client.guilds.size}');
+	client.channels.get(botStats.totalUsersID).setname('TotalUsers : ${client.guilds.reduce((a, g) => a + g.memberCount, 0)}');
+	client.channels.get(botStats.totalChannelsID).setname('TotalChannels : ${client.channels.size}');
+		
+			
+		
+	
+	
+	
+	
+	
+	});
 
 
-
-
-	client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
